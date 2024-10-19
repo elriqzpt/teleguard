c0283e8f0293a9e6e3c6cb1537e77cee33f88213
elriqzpt <134592956+elriqzpt@users.noreply.github.com>
2024-10-19 23:06:46 +0700

    Update and rename guard.php to index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 
 $botToken = '6542527750:AAF80sFqoIiDg36R8CYghgU5vrG5PaeWHIE';
 $botusername = 'DontUnsubRobot';
-$webhookUrl = 'https://api.telegram.org/bot6542527750:AAF80sFqoIiDg36R8CYghgU5vrG5PaeWHIE/setWebhook?url=https://teleguard.vercel.app/guard.php';
+$webhookUrl = 'https://api.telegram.org/bot6542527750:AAF80sFqoIiDg36R8CYghgU5vrG5PaeWHIE/setWebhook?url=https://teleguard.vercel.app';
 
 //no need to edit bellow
 
